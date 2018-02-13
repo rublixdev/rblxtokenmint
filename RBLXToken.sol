89e14e1100ca642e8a17bb4095fee08963d970b0
Peter <33762147+rblxpete@users.noreply.github.com>
2018-02-12 21:10:55 -0700

    Update RBLXToken.sol

--- RBLXToken.sol
+++ RBLXToken.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.19;
 contract tokenRecipient { 
     
-    function receiveApproval (address _from, uint256 _value, address _token, bytes _extraData)public; 
+    function receiveApproval (address _from, uint256 _value, address _token, bytes _extraData) public; 
     }
 
 contract RBLXToken {
